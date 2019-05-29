096b648232e49d7b09818ffd442913ffdfd727ef
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2019-05-29 10:04:48 +0530

    Update TippingGauge.ino

--- TippingGauge/TippingGauge.ino
+++ TippingGauge/TippingGauge.ino
@@ -1,14 +1,14 @@
-unsigned long mils1, mils2 = 0;
+unsigned long mils = 0;
 int count = 0;
 int pulse1, pulse2 = 0;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-pinMode (50,INPUT);
+pinMode (2,INPUT);
 }
 
 void loop() {
-  pulse1 = digitalRead(50);
+  pulse1 = digitalRead(2);
   
   if((pulse1-pulse2)== 1){
     count++;
@@ -16,13 +16,12 @@ void loop() {
   //Serial.print(pulse1);
   delay(10);
   pulse2 = pulse1;
-  Serial.print("Count = ");
-  Serial.println(count);
-  mils1 = millis();
-  if(mils1  == 2000){
+  //Serial.print("Count = ");
+  //Serial.println(count);
+  mils = millis();
+  if(mils%5000 < 10){
     Serial.print("Pulses in time =");
     Serial.println(count);
     count = 0;
-    mils2 += 2000;
   }
 }
